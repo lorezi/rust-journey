9361cc4bc939c4a70491abf0779e8153ca6221b0
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-05-31 13:33:46 +0100

    adds number generation

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,16 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    // let secret_number: u8 = random(1, 101);
+    println!("The secret number is: {}", secret_number);
+
     println!("Please input your guess.");
 
     let mut guess = String::new(); //mutable - The :: syntax in the ::new line indicates the new is an associated function of the String type.
-
-    println!("You guessed: {}", guess);
 
     io::stdin()
         .read_line(&mut guess)
